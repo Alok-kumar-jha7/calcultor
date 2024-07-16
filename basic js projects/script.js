90a135d624e0f163b38691d23e1d2098a573ad96
Alok kumar jha <aalok3828@gmail.com>
2024-07-16 20:57:29 +0530

    first commit

---
+++ basic js projects/script.js
@@ -0,0 +1,23 @@
+const blueButton = document.getElementById("blue");
+console.log(blueButton);
+blueButton.addEventListener('click', () => {
+    document.body.style.background = "blue";
+});
+
+const greenButton = document.getElementById("green");
+console.log(greenButton);
+greenButton.addEventListener('click', () => {
+    document.body.style.background = "green";
+});
+
+const redButton = document.getElementById("red");
+console.log(redButton);
+redButton.addEventListener('click', () => {
+    document.body.style.background = "red";
+});
+
+const yellowButton = document.getElementById("yellow");
+console.log(yellowButton);
+yellowButton.addEventListener('click', () => {
+    document.body.style.background = "yellow";
+});
